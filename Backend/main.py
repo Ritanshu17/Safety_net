932ae6832248a2034780d43b4cb10c1866fcdba9
Ritanshu17 <ritanshu755mankar@gmail.com>
2026-01-08 14:14:24 +0530

    inital commit

---
+++ Backend/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+import redis
+import time
+import random
+
+app = FastAPI()
+
+# Connect Redis
+cache = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+@app.get("/safety")
+def get_safety(lat: float, lon: float):
+    key = f"{lat}:{lon}"
+
+    # Check Redis cache
+    if cache.exists(key):
+        return {
+            "score": cache.get(key),
+            "cached": True
+        }
+
+    # Fake crime logic (replace with real API later)
+    time.sleep(1)
+    score = random.randint(1, 5)
+
+    cache.setex(key, 3600, score)
+
+    return {
+        "score": score,
+        "cached": False
+    }
+
